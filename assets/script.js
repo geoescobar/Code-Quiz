1626da4d792e49bd17a57d6bff8cd623d318c5a0
George Escobar <geo.escobar214@gmail.com>
2022-03-18 19:51:55 -0500

    homework update

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,26 @@
-// var startBtn = document.getElementById('start-btn');
-// var nextBtn = document.getElementById('next-btn');
-// var submitBtn = document.getElementById('submit-btn');
-// var answerA = document.getElementById('answer-btn-1');
-// var answerB = document.getElementById('answer-btn-2');
-// var answerC = document.getElementById('answer-btn-3');
-// var answerD = document.getElementById('answer-btn-4');
+var startBtn = document.getElementById('start-btn');
+var nextBtn = document.getElementById('next-btn');
+var submitBtn = document.getElementById('submit-btn');
+var answerA = document.getElementById('answer-btn-1');
+var answerB = document.getElementById('answer-btn-2');
+var answerC = document.getElementById('answer-btn-3');
+var answerD = document.getElementById('answer-btn-4');
+var container = document.getElementById('quiz-container');
+var displayQuestion = document.getElementById('display-questions');
+
+
+
+console.log(container)
+
+displayQuestion.style.display = 'none';
+nextBtn.style.display = 'none';
+submitBtn.style.display = 'none';
+answerA.style.display = 'none';
+answerB.style.display = 'none';
+answerC.style.display = 'none';
+answerD.style.display = 'none';
+
+
 
 startBtn.addEventListener('click', startGame);
 
@@ -44,7 +60,7 @@ var questions = [
 ]
 
 function endGame() {
-    alert ('Game is over');
+    alert('Game is over');
 
 }
 
@@ -53,43 +69,43 @@ function askNextQuestion() {
     if (quesitonIndex >= questions.length) {
         endGame();
     } else {
-        questionElement.textContent = questions[quesitonIndex].quesiton
-        questionElement.textContent = questions[quesitonIndex].answerA
-        questionElement.textContent = questions[quesitonIndex].answerB
-        questionElement.textContent = questions[quesitonIndex].answerC
-        questionElement.textContent = questions[quesitonIndex].answerD
-        questionElement.textContent = questions[quesitonIndex].correctAnswer
+        questionElement.textContent = questions[quesitonIndex].quesiton;
+        questionElement.textContent = questions[quesitonIndex].answerA;
+        questionElement.textContent = questions[quesitonIndex].answerB;
+        questionElement.textContent = questions[quesitonIndex].answerC;
+        questionElement.textContent = questions[quesitonIndex].answerD;
+        questionElement.textContent = questions[quesitonIndex].correctAnswer;
 
     }
 }
 
 var timeRemaining = 60;
 
-answerA.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('answer a has been clicked');
-    quesitonIndex++;
-    askNextQuestion();
-});
+// answerA.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('answer a has been clicked');
+//     quesitonIndex++;
+//     askNextQuestion();
+// });
 
-answerD.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('answer a has been clicked');
+// answerD.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('answer a has been clicked');
 
-    if (questions[quesitonIndex].correctAnswer === 'B') {
-        alert('Correct')
-    } else {
-        alert('Incorrect')
-    }
-    quesitonIndex++;
-    timeRemaining-= 10;
-    askNextQuestion();
-});
+//     if (questions[quesitonIndex].correctAnswer === 'B') {
+//         alert('Correct')
+//     } else {
+//         alert('Incorrect')
+//     }
+//     quesitonIndex++;
+//     timeRemaining -= 10;
+//     askNextQuestion();
+// });
 
 function startTimer() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         timeRemaining--;
-        
+
         if (timeRemaining === 0) {
             clearInterval(timerInterval);
             endGame();
@@ -102,11 +118,16 @@ function startTimer() {
 }
 
 function startGame() {
-    document.getElementById('start-btn').classList.toggle('hide');
-    document.getElementById('quiz-Container').classList.toggle('hide');
+    displayQuestion.style.display = 'block';
+    nextBtn.style.display = 'block';
+    submitBtn.style.display = 'block';
+    answerA.style.display = 'block';
+    answerB.style.display = 'block';
+    answerC.style.display = 'block';
+    answerD.style.display = 'block';
+    // container.classList.toggle('hide');
     askNextQuestion();
-    startTimer();    
+    startTimer();
 }
 
 startBtn.addEventListener('click', startGame);
-
